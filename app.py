e54b4888085840ce7005e84a291bb5f22920b149
D.MITHESH <114052780+Mithesh6369894902@users.noreply.github.com>
2025-04-05 20:50:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection 
 from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 from sklearn.metrics import accuracy_score
