78afbc7beb0ba1aded177fdb7926fddde1978423
D.MITHESH <114052780+Mithesh6369894902@users.noreply.github.com>
2025-04-06 17:20:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,87 @@
 
 
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn import svm
-from sklearn.metrics import accuracy_score
+import pickle
+
+pickle_in = open('Diabetes.pkl', 'rb')
+classifier = pickle.load(pickle_in)
+
+def predict():
+    st.sidebar.header('Diabetes Prediction')
+    # select = st.sidebar.selectbox('Select Form', ['Form 1'], key='1')
+    # if not st.sidebar.checkbox("Hide", True, key='2'):
+    st.title('Diabetes Prediction(Only for Females Above 21 Years of Age)')
+    st.markdown('This trained dataset is originally from the National Institute of Diabetes and Digestive and Kidney Diseases. The objective is to predict based on diagnostic measurements whether a patient has diabetes.')
+    st.markdown('Several constraints were placed on the selection of these instances from a larger database. In particular, all patients here are females at least 21 years old of Pima Indian heritage.')
+
+    name = st.text_input("Name:")
+    pregnancy = st.number_input("No. of times pregnant:")
+    st.markdown('Pregnancies: Number of times pregnant')
+
+    glucose = st.number_input("Plasma Glucose Concentration :")
+    st.markdown('Glucose: Plasma glucose concentration a 2 hours in an oral glucose tolerance test')
+
+    bp =  st.number_input("Diastolic blood pressure (mm Hg):")
+    st.markdown('BloodPressure: Diastolic blood pressure (mm Hg)')
+
+    skin = st.number_input("Triceps skin fold thickness (mm):")
+    st.markdown('SkinThickness: Triceps skin fold thickness (mm)')
+
+    insulin = st.number_input("2-Hour serum insulin (mu U/ml):")
+    st.markdown('Insulin: 2-Hour serum insulin (mu U/ml)')
 
 
+    bmi = st.number_input("Body mass index (weight in kg/(height in m)^2):")
+    st.markdown('BMI: Body mass index (weight in kg/(height in m)^2)')
 
-# Title
-st.title("Diabetes Prediction Web App")
-# Load Dataset
-@st.cache_data
-def load_data():
-    df = pd.read_csv("/content/drive/MyDrive/diabetes.csv")
-    return df
+    dpf = st.number_input("Diabetes Pedigree Function:")
+    st.markdown('DiabetesPedigreeFunction: Diabetes pedigree function')
 
-df = load_data()
-st.subheader("Sample Data")
-st.write(df.head())
 
-# Preprocessing
-X = df.drop(columns='Outcome', axis=1)
-Y = df['Outcome']
+    age = st.number_input("Age:")
+    st.markdown('Age: Age (years)')
 
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
+    submit = st.button('Predict')
+    st.markdown('Outcome: Class variable (0 or 1)')
 
-# Model training
-model = svm.SVC(kernel='linear')
-model.fit(X_train, Y_train)
 
-# Accuracy
-X_train_pred = model.predict(X_train)
-train_acc = accuracy_score(Y_train, X_train_pred)
+    if submit:
+        prediction = classifier.predict([[pregnancy, glucose, bp, skin, insulin, bmi, dpf, age]])
+        if prediction == 0:
+            st.write('Congratulation!',name,'You are not diabetic')
+        else:
+            st.write(name,", we are really sorry to say but it seems like you are Diabetic. But don't lose hope, we have suggestions for you:")
+            st.markdown('[Visit Here](https://www.mayoclinic.org/diseases-conditions/type-2-diabetes/in-depth/diabetes-prevention/art-20047639#:~:text=Diabetes%20prevention%3A%205%20tips%20for%20taking%20control%201,Skip%20fad%20diets%20and%20make%20healthier%20choices%20)')
 
-X_test_pred = model.predict(X_test)
-test_acc = accuracy_score(Y_test, X_test_pred)
 
-st.write("Training Accuracy:", round(train_acc * 100, 2), "%")
-st.write("Test Accuracy:", round(test_acc * 100, 2), "%")
+def main():
+    new_title = '<p style="font-size: 42px;">Welcome The Diabetes Prediction App!</p>'
+    read_me_0 = st.markdown(new_title, unsafe_allow_html=True)
+    read_me = st.markdown("""
+    The application is built using Streamlit  
+    to demonstrate Diabetes Prediction. It performs prediction on multiple parameters
+    [here](https://github.com/Priyanshu88/Diabetes-Prediction-Streamlit-App#signal_strength-dataset).""")
+    st.sidebar.title("Select Activity")
+    choice = st.sidebar.selectbox(
+        "MODE", ("About", "Predict Diabetes"))
+    if choice == "Predict Diabetes":
+        read_me_0.empty()
+        read_me.empty()
+        predict()
+    elif choice == "About":
+        print()
 
-# Sidebar for input
-st.sidebar.header("Input Patient Data")
 
-def user_input():
-    Pregnancies = st.sidebar.number_input('Pregnancies', 0, 20, step=1)
-    Glucose = st.sidebar.slider('Glucose', 0, 200, 100)
-    BloodPressure = st.sidebar.slider('BloodPressure', 0, 150, 70)
-    SkinThickness = st.sidebar.slider('SkinThickness', 0, 100, 20)
-    Insulin = st.sidebar.slider('Insulin', 0, 900, 80)
-    BMI = st.sidebar.slider('BMI', 0.0, 70.0, 25.0)
-    DiabetesPedigreeFunction = st.sidebar.slider('DiabetesPedigreeFunction', 0.0, 3.0, 0.5)
-    Age = st.sidebar.slider('Age', 15, 100, 30)
-
-    user_data = np.array([[Pregnancies, Glucose, BloodPressure, SkinThickness,
-                           Insulin, BMI, DiabetesPedigreeFunction, Age]])
-    return user_data
-
-input_data = user_input()
-
-# Prediction
-input_data_scaled = scaler.transform(input_data)
-prediction = model.predict(input_data_scaled)
-
-if st.button("Predict"):
-    if prediction[0] == 1:
-        st.error("The person is **diabetic**.")
-    else:
-        st.success("The person is **not diabetic**.")
+if __name__ == '__main__':
+    main()
